d406f3783ddedec5fc4263effe6a1d241c0b5d07
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2024-03-22 10:43:33 +0000

    simple learning project for testing out core java

---
+++ LearningJv/src/Main.java
@@ -0,0 +1,27 @@
+/**
+ A Try of the docs
+ */
+
+public class Main {
+
+    /**
+     Withdraws money from the bank account.
+     @param amount the amount to wothdraw
+     */
+    public void withdraw(double amount){
+        // implementation here
+    }
+
+    /**
+     Gets the current balance
+     @return the current balance
+     */
+    public double getBalance(){
+        return 0;
+    }
+    public static void main(String[] args) {
+
+    }
+
+
+}
