998b0aae532b33dec887ed30b12403a27207a26b
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2024-03-25 09:27:33 +0000

    added the restaurant entity

--- diningreviews/src/main/java/com/example/diningreviews/entity/Restaurant.java
+++ diningreviews/src/main/java/com/example/diningreviews/entity/Restaurant.java
@@ -2,57 +2,57 @@ package com.example.diningreviews.entity;
 
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+
+import java.util.Map;
+import java.util.Objects;
 
 @Entity
 @Table(name = "RESTAURANT")
+@Data
 public class Restaurant {
 
     @Id
-    @GeneratedValue
-    @Getter
-    @Setter
+    @GeneratedValue(strategy = GenerationType.AUTO) // the default
     private Long id;
 
-    @Column(name = "RESTAURANTNAME")
-    @Getter
-    @Setter
-    private String restaurantName;
+    @Column(name = "NAME")
+    private String name;
 
-    @Column(name = "PEANUTSCORE")
-    @Getter
-    @Setter
-    private Double peanutScore;
+    @ElementCollection // indicates that reviewScores is a collection of simple types (a map) rather than an entity
+    @CollectionTable(name = "restaurant_scores")//  the elements of the reviewScores map will be stored in a separate table named "restaurant_scores"
+    @MapKeyColumn(name = "score")// the keys of the reviewScores map (which represent the food allergy categories) will be stored in a column named "category".
+    @Column(name = "REVIEW SCORES")
+    private Map<String, Double> reviewScores;
 
+    public Restaurant(){}
 
-    @Column(name = "PEANUTNUMBER")
-    @Getter
-    @Setter
-    private Double peanutNumber;
+    public Restaurant(String name, Map<String, Double> reviewScores){
+        this.name = name;
+        this.reviewScores = reviewScores;
+    }
 
-    @Column(name = "EGGSCORE")
-    @Getter
-    @Setter
-    private Double eggScore;
+    // as a good practice
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Restaurant that)) return false;
+        return Objects.equals(this.getId(), that.getId()) &&
+                Objects.equals(this.getName(), that.getName()) &&
+                Objects.equals(this.getReviewScores(), that.getReviewScores());
+    }
 
-    @Column(name = "EGGNUMBER")
-    @Getter
-    @Setter
-    private Double eggNumber;
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId(), this.getName(), this.getReviewScores());
+    }
 
-    @Column(name = "DIARYSCORE")
-    private Double diaryScore;
-
-    @Column(name = "DIARYNUMBER")
-    @Getter
-    @Setter
-    private Double diaryNumber;
-
-    @Column(name = "OVERALLSCORE")
-    @Getter
-    @Setter
-    private Double overAllScore;
-
-
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "id=" + this.getId() +
+                ", name='" + this.getName() + '\'' +
+                ", reviewScores=" + this.getReviewScores() +
+                '}';
+    }
 }
