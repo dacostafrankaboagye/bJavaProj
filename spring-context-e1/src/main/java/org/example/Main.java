08ddfeee8188074b97cc5ec431c1d2806d6f6450
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2024-04-03 15:05:05 +0000

    spring

--- spring-context-e1/src/main/java/org/example/Main.java
+++ spring-context-e1/src/main/java/org/example/Main.java
@@ -1,52 +1,17 @@
 package org.example;
 
-import beansPac.MyBean;
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.ProductDeliveryService;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        /*
-        XML
-        Annotations
-         */
 
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(ProjectConfig.class); // as a parameter, you have to configure a spring application class
+        AnnotationConfigApplicationContext aCaContext = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
+        ProductDeliveryService service =  aCaContext.getBean(ProductDeliveryService.class);
 
-        MyBean myBean = context.getBean(MyBean.class);
-        MyBean myBean2 = context.getBean(MyBean.class);
-        MyBean myBean3 = context.getBean(MyBean.class);
-
-        System.out.println(myBean.getaText());
+        service.addSomeProduct();
 
     }
 }
-
-// by type
-        /*
-
-        MyBean b1 = context.getBean(MyBean.class);
-        MyBean b2 = context.getBean(MyBean.class);
-        System.out.println(b1.getSomeText());
-        System.out.println(b1.getSomeText());
-
-
-
-        // by name - we can avoid the error of multiple instance
-        MyBean otherOne = context.getBean("myOtherBean",MyBean.class);
-        System.out.println(otherOne.getSomeText());
-
-        MyBean b1 = context.getBean("myBean1", MyBean.class);
-        System.out.println(b1.getSomeText());
-
-
-
-        // using another name for the method @Bean("FrankBean")
-        MyBean fBean = context.getBean("FrankBean",MyBean.class);
-        System.out.println(fBean.getSomeText());
-
-         */
