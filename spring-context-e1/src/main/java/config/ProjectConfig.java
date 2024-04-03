4d9250851f12199661972beaf239031596f59a3b
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2024-04-03 10:35:56 +0000

    spring

--- spring-context-e1/src/main/java/config/ProjectConfig.java
+++ spring-context-e1/src/main/java/config/ProjectConfig.java
@@ -1,26 +1,50 @@
 package config;
 
-import beans.MyBean;
-import org.springframework.context.annotation.Bean;
+//import beans.MyBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
+
+@Configuration
+@ComponentScan(basePackages = "beansPac")
+public class ProjectConfig{
+
+}
+
+/*
 @Configuration  // tells spring that this is a configuration class
 public class ProjectConfig {
 
     // we want to return the instance of MyBean to be placed in the spring context
 
-    @Bean  // tells spring to place "MyBean (the instance)" in the spring context
-    public MyBean myBean(){
+   // @Bean  // tells spring to place "MyBean (the instance)" in the spring context
+    public MyBean myBean1(){
         MyBean b = new MyBean();
-        b.setSomeText("This is an example text");
+        b.setSomeText("For bean 1");
         return b;
     }
+    //@Primary  // if there are multiple instance -> it defaults to this because of the @Primary
     @Bean
-    @Primary
     public MyBean myBean2(){
         MyBean b2 = new MyBean();
-        b2.setSomeText("Here is the second one");
+        b2.setSomeText("the bean 2");
         return b2;
     }
+
+    @Bean
+    public MyBean myOtherBean(){
+        MyBean anotherOne = new MyBean();
+        anotherOne.setSomeText("another bean - myother bean");
+        return anotherOne;
+    }
+
+    // specify another name for the method
+    @Bean("FrankBean")
+    public MyBean frankBean(){
+        MyBean a = new MyBean();
+        a.setSomeText("frank bean");
+        return a;
+    }
+
 }
+*/
--- spring-context-e1/src/main/java/org/example/Main.java
+++ spring-context-e1/src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import beans.MyBean;
+import beansPac.MyBean;
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -16,11 +16,37 @@ public class Main {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(ProjectConfig.class); // as a parameter, you have to configure a spring application class
 
-        MyBean b1 = context.getBean(MyBean.class);
-//        MyBean b2 = context.getBean(MyBean.class);
-//
-//        System.out.println(b1.getSomeText());
-        System.out.println(b1.getSomeText());
+
+        MyBean myBean = context.getBean(MyBean.class);
+        MyBean myBean2 = context.getBean(MyBean.class);
+        MyBean myBean3 = context.getBean(MyBean.class);
+
+        System.out.println(myBean.getaText());
 
     }
 }
+
+// by type
+        /*
+
+        MyBean b1 = context.getBean(MyBean.class);
+        MyBean b2 = context.getBean(MyBean.class);
+        System.out.println(b1.getSomeText());
+        System.out.println(b1.getSomeText());
+
+
+
+        // by name - we can avoid the error of multiple instance
+        MyBean otherOne = context.getBean("myOtherBean",MyBean.class);
+        System.out.println(otherOne.getSomeText());
+
+        MyBean b1 = context.getBean("myBean1", MyBean.class);
+        System.out.println(b1.getSomeText());
+
+
+
+        // using another name for the method @Bean("FrankBean")
+        MyBean fBean = context.getBean("FrankBean",MyBean.class);
+        System.out.println(fBean.getSomeText());
+
+         */
