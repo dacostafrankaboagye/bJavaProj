9942e1b035fc6aa1aaab6d1d9b83b4ce79e4abb4
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2024-04-11 19:23:25 +0000

    added unit test for repository layer

--- rest-demo-jpa-mysql/src/main/java/com/frankaboagye/restdemojpamysql/repositories/CloudVendorRepository.java
+++ rest-demo-jpa-mysql/src/main/java/com/frankaboagye/restdemojpamysql/repositories/CloudVendorRepository.java
@@ -3,5 +3,12 @@ package com.frankaboagye.restdemojpamysql.repositories;
 import com.frankaboagye.restdemojpamysql.models.CloudVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CloudVendorRepository extends JpaRepository<CloudVendor, Integer> {
+    List<CloudVendor> findByVendorName(String vendorName);
 }
+
+
+// there is no need to write test cases - for those which we are directly using
+// we can write a test of the findByVendorName
---
+++ rest-demo-jpa-mysql/src/test/java/com/frankaboagye/restdemojpamysql/services/serviceImplementations/CloudVendorServiceImplementationTest.java
@@ -0,0 +1,44 @@
+package com.frankaboagye.restdemojpamysql.services.serviceImplementations;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class CloudVendorServiceImplementationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createCloudVendor() {
+    }
+
+    @Test
+    void updateCloudVendor() {
+    }
+
+    @Test
+    void deleteCloudVendor() {
+    }
+
+    @Test
+    void getCloudVendor() {
+    }
+
+    @Test
+    void getAllCloudVendors() {
+    }
+}
+
+/*
+
+- We are not supposed to make the service layer talk to the database
+- So how do we get database responses?
+    - Answer: Mock it
+ */
