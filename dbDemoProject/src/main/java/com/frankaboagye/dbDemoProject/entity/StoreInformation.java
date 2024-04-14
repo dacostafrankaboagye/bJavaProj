6d6be460d607952679d356a78508e9cdec658e7f
dacostafrankaboagye <frank.aboagye@ashesi.edu.gh>
2024-04-14 21:01:30 +0000

    added springDatajpa

--- dbDemoProject/src/main/java/com/frankaboagye/dbDemoProject/entity/StoreInformation.java
+++ dbDemoProject/src/main/java/com/frankaboagye/dbDemoProject/entity/StoreInformation.java
@@ -1,2 +1,35 @@
-package com.frankaboagye.dbDemoProject.entity;public class StoreInformation {
+package com.frankaboagye.dbDemoProject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class StoreInformation {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer storeId;
+    private String storeName;
+    private String storeDetail;
+    private String storePhoneNumber;
+
+    public StoreInformation(String storeName, String storeDetail, String storePhoneNumber) {
+        this.storeName = storeName;
+        this.storeDetail = storeDetail;
+        this.storePhoneNumber = storePhoneNumber;
+    }
+
+
 }
--- dbDemoProject/src/main/java/com/frankaboagye/dbDemoProject/repository/StoreInformationRepository.java
+++ dbDemoProject/src/main/java/com/frankaboagye/dbDemoProject/repository/StoreInformationRepository.java
@@ -1,2 +1,17 @@
-package com.frankaboagye.dbDemoProject.repository;public class StoreInformationRepository {
+package com.frankaboagye.dbDemoProject.repository;
+
+import com.frankaboagye.dbDemoProject.entity.StoreInformation;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface StoreInformationRepository extends CrudRepository<StoreInformation, Integer> {
+
+    // the name mapping should happen
+
+    List<StoreInformation> findByStoreName(String storeName);
+    List<StoreInformation> findByStoreDetail(String storeDetail);
+    List<StoreInformation> findByStorePhoneNumber(String storePhoneNumber);
+
+
 }
